918adcebbc7880f5d0bbb70219055cd4d4a57cf0
Alsalira <liraalsaamalia@gamil.com>
2022-07-21 09:18:54 +0700

    update file

--- pages/admin/dokumen/bpjskesehatan.php
+++ pages/admin/dokumen/bpjskesehatan.php
@@ -1,22 +1,6 @@
-<?php
-    // require 'functions.php';
-
-    // $users = query("SELECT * FROM users");
-
-    // koneksi ke database
-    $conn = mysqli_connect("localhost", "root", "", "alur_bandung");
-
-    // ambil data dari tabel user
-    $result = mysqli_query($conn, "SELECT * FROM users");
-    
-    // ambil data (fetch) dari objek result (assoc: mengembalikan array associative)
-    $users = mysqli_fetch_assoc($result);
-
-?>
-
 <!DOCTYPE html>
 <html lang="en">
-<head> 
+<head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
@@ -25,31 +9,31 @@
     <link href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
     
     <!-- my css -->
-    <link rel="stylesheet" type="text/css" href="../assets/css/ijazah.css">
+    <link rel="stylesheet" type="text/css" href="../../../assets/css/ijazah.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
 
-    <title>Dokumen Ijazah</title>
+    <title>Dokumen BPJS KESEHATAN</title>
 </head>
 <body>
-    <div class="body-table">
-    <table class="table table-bordered" style="text-align: center">
+    <table class="table table-bordered table-a">
     <thead>
         <tr>
-            <th scope="col">Nama</th>
-            <th scope="col">Dokumen</th>
+        <th scope="col">Nama</th>
+        <th scope="col">Dokumen</th>
         </tr>
     </thead>
-    <tbody>
-        <?php while( $users = mysqli_fetch_assoc($result) ) :  ?>
+    <tbody class="edit-tbody">
         <tr>
-            <th scope="row"><?= $users["nama"]; ?></th>
-            <td class="td"><a class="btn btn-danger button-lihat" href="" role="button">Download</a></td>
+        <th scope="row">Mark</th>
+        <td><a class="btn btn-danger button-lihat" href="" role="button">Download</a></td>
         </tr>
-        <?php endwhile; ?>
+        <tr>
+        <th scope="row" >Jacob</th>
+        <td><a class="btn btn-danger button-lihat" href="" role="button">Download</a></td>
+        </tr>
     </tbody>
     </table>
-    </div>
     <a href="../pages/datadokumen.php" class="btn btn-kembali" type="button" style="border-radius: 17px; width: 110px; margin-left: 10px; margin-bottom: 20px; background-color: #063554; color: white; font-size: 17px; text-transform: uppercase">Kembali</a>
 </body>
 </html>
