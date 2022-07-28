c789bbb188815d9e9e7e7075ee260bad78b1e3fa
Alsalira <liraalsaamalia@gamil.com>
2022-07-28 08:48:48 +0700

    update halaman profile

--- pages/download.php
+++ pages/download.php
@@ -3,32 +3,25 @@ require_once __DIR__ . '../../vendor/autoload.php';
 
 require 'functions.php';
 
-$users = query("SELECT users.ijazah, nama, id_user FROM users");
+$users = query("SELECT users.ijazah, id_user FROM users");
 
 $mpdf = new \Mpdf\Mpdf();
-$html = '<!DOCTYPE html>
-<html lang="en">
+// $html = '<!DOCTYPE html>
+// <html lang="en">
 
-<head>
-  <meta charset="UTF-8" />
-  <title>Download Ijazah</title>
-</head>
-<body>';
-foreach ($users as $lu) :
-    ?>
-      <tr>
-        <td id="act-icon">
-          <div class="action">
-            <a href="detail.php?id=<?= $lu["ijazah"]; ?>">
-            </a>
-          </div>
-        </td>
-      </tr>
-    <?php endforeach; ?>
-$html .= '</body>
+// <head>
+//   <meta charset="UTF-8" />
+//   <title>Download Ijazah</title>
+// </head>
+// <body>';
 
-</html>';
+//   echo $users["ijazah"]; ?>
 
+<!-- // $html .= '</body>
+
+// </html>'; -->
+
+$html = 
 $mpdf->WriteHTML($html);
 $mpdf->Output();
 
