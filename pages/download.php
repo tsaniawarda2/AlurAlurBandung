f1de2494a8bb023508299ebb48d8fec3fbf83f2e
Alsalira <liraalsaamalia@gamil.com>
2022-07-31 14:34:47 +0700

    update halaman download

--- pages/download.php
+++ pages/download.php
@@ -1,11 +1,21 @@
 <?php
 require_once __DIR__ . '../../vendor/autoload.php';
 
-// require 'functions.php';
+require 'functions.php';
 
-// $users = query("SELECT users.ijazah, id_user FROM users");
+$users = query("SELECT users.ijazah, id_user FROM users");
 
 $mpdf = new \Mpdf\Mpdf();
-$mpdf->WriteHTML('<h1>Halo Alsa!</h1>');
+$html = '<html>
+  <head>
+    <title>Dokumen Ijazah</title>
+  </head>
+  <body>';
+     
+  echo $users["ijazah"];
+
+ $html .= '</body>
+</html>';
+$mpdf->WriteHTML($html);
 $mpdf->Output();
 
