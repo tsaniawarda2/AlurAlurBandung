6574febc2e11f104d3f2f313460c73e61bb25a87
Alsalira <liraalsaamalia@gamil.com>
2022-08-01 10:20:20 +0700

    update profilee

--- pages/uploadfile.php
+++ pages/uploadfile.php
@@ -1,6 +1,42 @@
 <?php
   require 'functions.php';
 
+  if (isset($_POST['simpan']))
+ {
+     $filename = $_FILES['ijazah']['ijazah'];
+     $destination = 'files/' . $filename;
+
+     $extention = pathinfo($filename,PATHINFO_EXTENSION);
+
+     $file = $_FILES['ijazah']['tmp_name'];
+     $size = $_FILES['ijazah']['size'];
+
+     if(!in_array($extention,['pdf']))
+     {
+         echo "Your file extention must be .pdf";
+     }
+     elseif($_FILES['ijazah']['size'] > 1000000) 
+     {
+         echo "file is too large";
+     }
+
+   else{
+        if(move_uploaded_file($file,$destination))
+        {
+            $sql = "INSERT INTO alur_bandung (ijazah)
+            VALUES('$filename')";
+
+            if(mysqli_query($connect,$sql))
+            {
+                echo "file uploaded succesfully";
+            }
+            else {
+                echo "failed to upload file";
+            }
+        }
+    }    
+ }
+
   // pengecekan tipe harus pdf
   //  $tipe_file = $_FILES['file']['type'];
   //  if ($tipe_file == "application/pdf") 
@@ -80,45 +116,50 @@
               <div class="card">
                 <div class="card-body p-md-5 mx-md-4">
 
-                  <form class="form-profile" action="" methode="post" enctype="multipart/form-data">
+                  <form class="form-profile" action="uploadfile.php" methode="post" enctype="multipart/form-data">
                     <p class="text-header text-center pt-3">Lengkapi Profile</p>
-  
-                    <div class="form-outline mb-2 mt-4 py-1">
-                      <label class="form-label text-simpan" for="form2Example11">Ijazah</label>
-                      <input type="file" name="ijazah" class="form-control" id="inputGroupFile01">
+
+                    <label class="form-label text-simpan" for="form2Example11">Ijazah</label>
+                    <div class="input-group">
+                      <input type="file" name="ijazah" class="form-control" id="inputGroupFile04" aria-describedby="inputGroupFileAddon04" aria-label="Upload">
+                      <button class="btn btn-outline-secondary" type="simpan" type="simpan" id="inputGroupFileAddon04" style="background-color: #063554; color: white">Simpan</button>
                     </div>
 
-                    <div class="form-outline mb-2 py-1">
-                      <label class="form-label text-simpan" for="form2Example11">KTP</label>
-                      <input type="file" name="ktp" class="form-control" id="inputGroupFile01">
+                    <label class="form-label text-simpan" for="form2Example11">KTP</label>
+                    <div class="input-group">
+                      <input type="file" name="ktp" class="form-control" id="inputGroupFile04" aria-describedby="inputGroupFileAddon04" aria-label="Upload">
+                      <button class="btn btn-outline-secondary" type="simpan" type="simpan" id="inputGroupFileAddon04" style="background-color: #063554; color: white">Simpan</button>
                     </div>
 
-                    <div class="form-outline mb-2 py-1">
-                      <label class="form-label text-simpan" for="form2Example11">BPJS KETENAGAKERJAAN</label>
-                      <input type="file" name="bpjs_ketenagakerjaan" class="form-control" id="inputGroupFile01">
+                    <label class="form-label text-simpan" for="form2Example11">BPJS KETEAGAKERJAAN</label>
+                    <div class="input-group">
+                      <input type="file" name="bpjs_ketenagakerjaan" class="form-control" id="inputGroupFile04" aria-describedby="inputGroupFileAddon04" aria-label="Upload">
+                      <button class="btn btn-outline-secondary" type="simpan" type="simpan" id="inputGroupFileAddon04" style="background-color: #063554; color: white">Simpan</button>
                     </div>
 
-                    <div class="form-outline mb-2 py-1">
-                      <label class="form-label text-simpan" for="form2Example11">BPJS KESEHATAN</label>
-                      <input type="file" name="bpjs_kesehatan" class="form-control" id="inputGroupFile01">
+                    <label class="form-label text-simpan" for="form2Example11">BPJS KESEHATAN</label>
+                    <div class="input-group">
+                      <input type="file" name="bpjs_kesehatan" class="form-control" id="inputGroupFile04" aria-describedby="inputGroupFileAddon04" aria-label="Upload">
+                      <button class="btn btn-outline-secondary" type="simpan" type="simpan" id="inputGroupFileAddon04" style="background-color: #063554; color: white">Simpan</button>
                     </div>
 
-                    <div class="form-outline mb-2 py-1">
-                      <label class="form-label text-simpan" for="form2Example11">NPWP</label>
-                      <input type="file" name="npwp" class="form-control" id="inputGroupFile01">
+                    <label class="form-label text-simpan" for="form2Example11">NPWP</label>
+                    <div class="input-group">
+                      <input type="file" name="npwp" class="form-control" id="inputGroupFile04" aria-describedby="inputGroupFileAddon04" aria-label="Upload">
+                      <button class="btn btn-outline-secondary" type="simpan" type="simpan" id="inputGroupFileAddon04" style="background-color: #063554; color: white">Simpan</button>
                     </div>
 
-                    <div class="form-outline mb-2 py-1">
-                      <label class="form-label text-simpan" for="form2Example11">KARTU KELUARGA</label>
-                      <input type="file" name="kk" class="form-control" id="inputGroupFile01">
+                    <label class="form-label text-simpan" for="form2Example11">KARTU KELUARGA</label>
+                    <div class="input-group">
+                      <input type="file" name="kk" class="form-control" id="inputGroupFile04" aria-describedby="inputGroupFileAddon04" aria-label="Upload">
+                      <button class="btn btn-outline-secondary" type="simpan" type="simpan" id="inputGroupFileAddon04" style="background-color: #063554; color: white">Simpan</button>
                     </div>
   
                     <div class="text-center pt-1 mb-3 pb-1">
-                      <button class="btn btn-simpan" name="simpan" type="simpan" style="background-color: #063554; color: white; border-radius: 17px; width: 100px; margin-top: 40px; text-transform: uppercase">Simpan</button>
-                        <br>
-                      <!-- <a class="text-muted" href="#!">Forgot password?</a> -->
+                    <a href="../index.php" class="btn-kembali" type="button"  style="background-color: #063554; color: white; border-radius: 17px; width: 100px; margin-top: 40px; text-transform: uppercase; padding: 7px; text-decoration: none">Kembali</a>
                     </div>
-                  </form>
+
+                   </form>
   
                 </div>
               </div>
